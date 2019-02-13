AmulyaX/android_device_xiaomi_lavender

jasmine: rebrand to jasmine_sprout and follow xiaomi name

AndroidProducts.mk
@@ -23,4 +23,4 @@
 
 # inherit MI A2 HAVOC device config
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/havoc_jasmine.mk
+    $(LOCAL_DIR)/havoc_jasmine_sprout.mk

vendorsetup.sh
@@ -21,6 +21,6 @@
 # definition file).
 #
 
- add_lunch_combo havoc_jasmine-eng
- add_lunch_combo havoc_jasmine-userdebug
- add_lunch_combo havoc_jasmine-user
+ add_lunch_combo havoc_jasmine_sprout-eng
+ add_lunch_combo havoc_jasmine_sprout-userdebug
+ add_lunch_combo havoc_jasmine_sprout-user

Runner85sx/android_device_xiaomi_jasmine_sprout

AndroidProducts.mk
@@ -23,4 +23,5 @@
 
 # inherit MI A2 HAVOC device config
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/carbon_jasmine.mk
+    $(LOCAL_DIR)/carbon_jasmine_sprout.mk
+

vendorsetup.sh
@@ -21,6 +21,7 @@
 # definition file).
 #
 
- add_lunch_combo carbon_jasmine-eng
- add_lunch_combo carbon_jasmine-userdebug
- add_lunch_combo carbon_jasmine-user
+ add_lunch_combo carbon_jasmine_sprout-eng
+ add_lunch_combo carbon_jasmine_sprout-userdebug
+ add_lunch_combo carbon_jasmine_sprout-user
+